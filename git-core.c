d34af005815abd2b7e5b5cc2a5fb6fe05c1a88be
Matias Linares <matiaslina@gmail.com>
2013-03-24 16:22:13 -0300

    Cambiado tabs por espacios

--- git-core.c
+++ git-core.c
@@ -10,7 +10,7 @@ extern git_repository* git_core_current_repository;
 
 static void get_hex_oid (gchar **uid, gchar *branch_file)
 {
-	g_return_if_fail (branch_file != NULL);
+    g_return_if_fail (branch_file != NULL);
     gboolean result;
     GError *err = NULL;
     
@@ -29,58 +29,58 @@ static void get_hex_oid (gchar **uid, gchar *branch_file)
 
 commit_info *git_core_commit_info_new (git_commit *commit)
 {
-	commit_info *info;
-	if((info = malloc(sizeof *info)) != NULL)
-	{
-		info->author = git_commit_author (commit);
-		info->message = git_commit_message (commit);
-		info->author = git_commit_author (commit);
-		info->committer = git_commit_committer (commit);
-		info->ctime = git_commit_time (commit);
+    commit_info *info;
+    if((info = malloc(sizeof *info)) != NULL)
+    {
+        info->author = git_commit_author (commit);
+        info->message = git_commit_message (commit);
+        info->author = git_commit_author (commit);
+        info->committer = git_commit_committer (commit);
+        info->ctime = git_commit_time (commit);
 
-		info->parent_count = git_commit_parentcount (commit);
-	}
+        info->parent_count = git_commit_parentcount (commit);
+    }
 
-	return info;
-}	
+    return info;
+}    
 
 gchar *git_core_create_commit ( const gchar *author_name,
-								const gchar *author_email,
-								const gchar *committer_name,
-								const gchar *committer_email,
-								gchar *message,
-								gchar *encoding,
-								const git_tree *tree,
-								const git_commit *parent,
-								int parent_count)
+                                const gchar *author_email,
+                                const gchar *committer_name,
+                                const gchar *committer_email,
+                                gchar *message,
+                                gchar *encoding,
+                                const git_tree *tree,
+                                const git_commit *parent,
+                                int parent_count)
 {
-	static char out[41];
-	git_oid commit_id;
-	git_signature *author, *committer;
+    static char out[41];
+    git_oid commit_id;
+    git_signature *author, *committer;
 
-	git_signature_now ((git_signature **) &author,
-					   author_name, author_email);
-	git_signature_now ((git_signature **) &committer,
-					   committer_name,
-					   committer_email);
+    git_signature_now ((git_signature **) &author,
+                       author_name, author_email);
+    git_signature_now ((git_signature **) &committer,
+                       committer_name,
+                       committer_email);
 
-	git_commit_create_v( &commit_id,                  // out id
-						 git_core_current_repository, // Repository
-						 NULL,                        // (update_ref) do not update the ref
-						 author,                      // Author of the commit
-						 committer,                   // Committer 
-						 encoding,                    // Encoding
-						 message,                     // Message
-						 tree,                        // Parent tree
-						 1,                           // parent count
-						 parent);                     // List the parents, we can have a lot of here.
+    git_commit_create_v( &commit_id,                  // out id
+                         git_core_current_repository, // Repository
+                         NULL,                        // (update_ref) do not update the ref
+                         author,                      // Author of the commit
+                         committer,                   // Committer 
+                         encoding,                    // Encoding
+                         message,                     // Message
+                         tree,                        // Parent tree
+                         1,                           // parent count
+                         parent);                     // List the parents, we can have a lot of here.
 
-	/* Free all the signatures */
-	git_signature_free (author);
-	git_signature_free (committer);
-	git_oid_fmt (out, &commit_id);
+    /* Free all the signatures */
+    git_signature_free (author);
+    git_signature_free (committer);
+    git_oid_fmt (out, &commit_id);
 
-	return out;
+    return out;
 }
 
 void git_core_load_repository (const gchar *path)
@@ -106,13 +106,13 @@ int main (int argc, char **argv)
     git_commit *commit;
     git_commit_lookup (&commit, git_core_current_repository, &oid);
     
-	commit_info *info = git_core_commit_info_new (commit);
+    commit_info *info = git_core_commit_info_new (commit);
     
     printf("Author: %s (%s)\n", info->author->name, info->author->email);
     
     git_commit_free (commit);
     
-	
+    
     git_repository_free (git_core_current_repository);
     return 0;
 }
